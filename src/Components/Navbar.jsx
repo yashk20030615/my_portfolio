3d21aa3bbfa4a7e07255525a695230aa9a68243c
Yash Kumar Gandhir <yashkumargandhir@gmail.com>
2026-06-19 23:02:35 +0530

    Updated navbar and resume

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
-import resumePDF from "../assets/Resume-Yash Kumar[Latest].pdf";
+import resumePDF from "../assets/Yash-Resume.pdf";
 import "./Navbar.css";
 
 const Navbar = () => {
